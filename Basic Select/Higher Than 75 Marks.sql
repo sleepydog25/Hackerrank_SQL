c6669edf914a8b815f9ae40d969febbabe0430e2
sleepydog25 <124705401+sleepydog25@users.noreply.github.com>
2024-03-26 01:18:29 +0800

    Create Higher Than 75 Marks.sql

---
+++ Basic Select/Higher Than 75 Marks.sql
@@ -0,0 +1,11 @@
+/*
+Higher Than 75 Marks
+
+Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+full question here:
+https://www.hackerrank.com/challenges/more-than-75-marks/problem?isFullScreen=true
+*/
+
+SELECT NAME FROM STUDENTS WHERE MARKS>75 
+ORDER BY RIGHT(Name, 3), ID ASC; ---MYSQL 
