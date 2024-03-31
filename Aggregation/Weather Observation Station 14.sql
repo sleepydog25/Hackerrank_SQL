672fe1382b6a57cea21abe7311bb06c8c3ca8cd7
Sleepy_dog <124705401+sleepydog25@users.noreply.github.com>
2024-03-31 12:34:30 +0800

    Create Weather Observation Station 14.sql

---
+++ Aggregation/Weather Observation Station 14.sql
@@ -0,0 +1,11 @@
+/*
+Weather Observation Station 14
+
+Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345 . Truncate your answer to 4 decimal places.
+
+full question here:
+https://www.hackerrank.com/challenges/weather-observation-station-14/problem?isFullScreen=true
+*/
+
+SELECT ROUND(MAX(LAT_N),4)FROM STATION
+WHERE LAT_N<137.2345;
